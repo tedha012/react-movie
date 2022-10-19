a7205f6112adaafae9f96124b31ac8ce34f5a644
tedha012 <tedha012@gmail.com>
2022-10-19 18:33:11 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
+
   const iRunOnlyOnce = () => {
     console.log("i run only once");
   };
   useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run only when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when keyword and counter changes");
+  }, [keyword, counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search now!"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me!</button>
     </div>
