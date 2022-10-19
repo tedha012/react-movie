76f042aae6b96a29b51da85ec1dfa9aeeead8864
tedha012 <tedha012@gmail.com>
2022-10-19 18:59:06 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import { useEffect, useState } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => {
+      console.log("bye :(");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
-
-  const iRunOnlyOnce = () => {
-    console.log("i run only once");
-  };
-  useEffect(iRunOnlyOnce, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("i run only when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("i run when keyword and counter changes");
-  }, [keyword, counter]);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search now!"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
     </div>
   );
 }
