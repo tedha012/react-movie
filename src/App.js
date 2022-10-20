f379a8bdd27ba1fc5a206c0f634d6fbeb4fe0a76
tedha012 <tedha012@gmail.com>
2022-10-20 15:48:22 +0900

    #7.7 Publishing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/movie/:id" element={<Detail />} />
-      <Route path="/" element={<Home />} />
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
     </Routes>
   );
 }
